7a3fc7822f6c1634d5ce8f46a36b33f0e3cd5bcd
aragonet <c.aragon.seb@gmail.com>
2020-04-11 12:26:33 +0200

    Push build image too

---
+++ camelapp/build/web/flutter_service_worker.js
@@ -0,0 +1,40 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "ba1a1c4bfe16e7bc3f184e7c019dca15",
+"assets/assets/board.jpg": "0ade8c145369b85613fff922d92c72b1",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "b62464f299ae2ec57e38a88317f9c11b",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "15e67b5ec01943fa65821f339394c68f",
+"/": "15e67b5ec01943fa65821f339394c68f",
+"main.dart.js": "30a3ec6abb3e13b91f6b5bcebec0eb21",
+"manifest.json": "0eb96b185c99d2f36469493d56e45793"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
